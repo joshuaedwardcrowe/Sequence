6578b88101037092343ca9df390de47021b373ab
Joshua Crowe <thejoshuacrowe@gmail.com>
2019-01-10 22:15:01 +0000

    D-1: Regenerated source, typings, documentation.

--- test.unit/operations/SelectionOperation.test.ts
+++ test.unit/operations/SelectionOperation.test.ts
@@ -5,6 +5,7 @@ import {SequenceColumn} from "../../src/SequenceColumn";
 import {Predicate} from "../../src/enums/Predicate";
 // Tested import
 import {ColumnOperation} from "../../src/operations/ColumnOperation";
+import {Operation} from "../../src/enums/Operation";
 
 // Test data
 let columnA: SequenceColumn;
@@ -19,11 +20,11 @@ describe("ColumnOperation",  () => {
     describe("Instance Methods",  () => {
         describe("stringify", () => {
             it("Stringifies a SELECT * statement", () => {
-                const operation: ColumnOperation = new ColumnOperation();
+                const operation: ColumnOperation = new ColumnOperation(Operation.Select);
                 expect(operation.stringify()).to.equal("SELECT *");
             });
             it("Stringifies a SELECT columnA, columnB statement", () => {
-                const operation: ColumnOperation = new ColumnOperation();
+                const operation: ColumnOperation = new ColumnOperation(Operation.Select);
                 operation.columns.push(columnA, columnB);
                 expect(operation.stringify()).to.equal("SELECT columnA, COUNT(columnB)");
             });
